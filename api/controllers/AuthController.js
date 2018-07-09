bfb3054e4d6dcaf69b13798eb70f73f9a167fa4e
Oli Slade <oliver.slade@digital.hmrc.gov.uk>
2018-07-09 12:38:56 +0100

    renaming json import

--- api/controllers/AuthController.js
+++ api/controllers/AuthController.js
@@ -1,5 +1,5 @@
 var axios = require('axios');
-var data = require('../resources/user.json')
+var data = require('../resources/token.json')
 
 exports.data = async (req, res) => {
     res.json(data);
